0220743e87ea2965ed0b2c226a4f3279ff82c143
GabrielBritoAlmeida <gabrielfranccelino@gmail.com>
2020-01-01 21:42:27 -0300

    Create de agendamento ok

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,42 @@
+import * as Yup from 'yup';
+import Appointment from '../models/Appointments';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados de agendamento inválido!' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    /**
+     * Verifica se provider_id é um provider
+     */
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(401)
+        .json({ error: 'Não encontrado um prestador de serviço!' });
+    }
+
+    // Criando agendamento
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserContorller';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewarers/auth';
 
@@ -23,6 +24,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
